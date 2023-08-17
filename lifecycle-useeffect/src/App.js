8ed90c7105334b37aa9fcfd74afb5eda4577de23
burhan <alacaburhan122016@gmail.com>
2023-08-17 22:51:06 +0300

    LifeCycle - useEffect

---
+++ lifecycle-useeffect/src/App.js
@@ -0,0 +1,24 @@
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [ show, setShow] = useState(false)
+
+
+  return (
+    <>
+      <div className="App">
+
+<h1>Yaşam Döngüsü - LifeCycle</h1>
+</div>
+<button onClick={()=>setShow((show)=> !(show) )}>
+{show ? "Gizle" : " Göster"}
+</button>
+<p className='read-the-docs'>Merhaba</p>
+    </>
+  );
+}
+
+export default App;
---
+++ lifecycle-useeffect/src/components/LifeCycle.jsx
@@ -0,0 +1,11 @@
+
+
+import React from 'react'
+
+const LifeCycle = () => {
+  return (
+    <div>LifeCycle</div>
+  )
+}
+
+export default LifeCycle
